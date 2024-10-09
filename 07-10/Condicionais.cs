93e3285af5c9e887eab57ea17ba0df05393f5095
LeoSouzaSenac <152904287+LeoSouzaSenac@users.noreply.github.com>
2024-10-09 17:11:55 -0300

    Add files via upload

--- 07-10/Condicionais.cs
+++ 07-10/Condicionais.cs
@@ -6,6 +6,7 @@ class Condicionais
 {
 
     static void Main (string[] args) {
+        /*
         bool temCarteira = true;
         bool maiorIdade = 29 >= 18;
         //SE eu tenho carteira
@@ -17,6 +18,46 @@ class Condicionais
             Console.WriteLine("Você é maior de idade, mas não tem carteira. Portanto, não pode dirigir.");
         } else {
             Console.WriteLine("Você não pode dirigir.");
-        }       
+        }     
+
+        */
+
+        string nacionalidade = "brasileiro";
+        
+        if (nacionalidade == "brasileiro"){
+            Console.WriteLine("E aí cpx");
+        } else if (nacionalidade == "americano"){
+            Console.WriteLine("Você disse petróleo?");
+        } else if (nacionalidade == "russo"){
+            Console.WriteLine("Cade os ucranianos?");
+        } else {
+            Console.WriteLine("Nacionalidade não encontrada.");
+        }
+        
+
+        switch (nacionalidade){
+
+            case "brasileiro":
+                Console.WriteLine("E aí cpx");
+            break;
+
+            case "americano":
+                Console.WriteLine("Você disse petróleo?");
+            break;
+
+            case "russo":
+                Console.WriteLine("Cade os ucranianos?");
+            break;
+
+            default:
+                Console.WriteLine("Nacionalidade não encontrada.");
+            break;
+        }
+
+
+        //Usando Switch, crie uma variável chamada meuTime
+        // faça um switch com cada case representando um time, e imprima algo diferente para cada um
+        //Não esqueça do default
+
     }  
 }
